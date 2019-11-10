0529c6101823f6b2eef1c3d225942ea4884d7117
Zeshan Raja <zraja4402@gmail.com>
2019-11-10 22:41:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -50,7 +50,7 @@ def matrix_update(matrix, row, column, new_value)
      ["Gerhard Schtitt","Hal Incandenza", "Lyle" , "Mario Incandenza", "Michael Pemulis"]
      ]
  
- arrayLiteral
+ arrayLiteral = arrayLiteral.delete([1])
  
  #change last element of first array to bruceGreen
  bruceGreen = arrayLiteral.shift()
