d569304ff8781627c1111ea3d2328009ce57d2b1
Zeshan Raja <zraja4402@gmail.com>
2019-11-10 23:36:27 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,12 +49,10 @@ def matrix_update(matrix, row, column, new_value)
      ["Bruce Green","Don Gately", "Joelle van Dyne", "Kate Gompert", "Pat M."],
      ["Gerhard Schtitt","Hal Incandenza", "Lyle" , "Mario Incandenza", "Michael Pemulis"]
      ]
-bruceGreen = arrayLiteral[0].shift
+
 #bruceGreen = arrayLiteral.shift 
 # arrayLiteral[0] returns first array
-#change last element of first array to bruceGreen
-arrayLiteral[0][3] = bruceGreen
-p arrayLiteral[0]
+
 
 
  
