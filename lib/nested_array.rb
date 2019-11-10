b93860c0505784257131097c4db8dd555db173a9
Zeshan Raja <zraja4402@gmail.com>
2019-11-10 22:59:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -53,5 +53,6 @@ def matrix_update(matrix, row, column, new_value)
  #change last element of first array to bruceGreen
  bruceGreen = arrayLiteral.shift
  arrayLiteral[4] = bruceGreen
+ p arrayLiteral
  
 end
